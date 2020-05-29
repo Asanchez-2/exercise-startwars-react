5be91c798ac8d70a56376517bfe76f5dbc0d5009
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-05-29 16:33:07 +0000

    Se añade la plantilla CardVehicles

--- src/js/component/CardVehicles.js
+++ src/js/component/CardVehicles.js
@@ -3,23 +3,49 @@ import { Context } from "../store/appContext";
 
 const CardVehicles = () => {
 	const { store, actions } = useContext(Context);
+	//return (
+	// <div className="text-center mt-5" style={{ width: "32%", position: "absolute", left: "34%" }}>
+	// 	<table className="table table-bordered mt-2 pt-5">
+	// 		<thead className="thead-dark">
+	// 			<tr>
+	// 				<th scope="col">Vehicles</th>
+	// 			</tr>
+	// 		</thead>
+	// 		<tbody className="tbody-dark">
+	// 			{store.planets.map((e, index) => (
+	// 				<tr key={index}>
+	// 					<td>{e.name}</td>
+	// 				</tr>
+	// 			))}
+	// 		</tbody>
+	// 	</table>
+	// </div>
 	return (
-		<div className="text-center mt-5" style={{ width: "32%", position: "absolute", left: "34%" }}>
-			<table className="table table-bordered mt-2 pt-5">
-				<thead className="thead-dark">
-					<tr>
-						<th scope="col">People</th>
-					</tr>
-				</thead>
-				<tbody className="tbody-dark">
-					{store.vehicles.map((e, index) => (
-						<tr key={index}>
-							<td>{e.name}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+		<React.Fragment>
+			<div className="row">
+				{store.vehicles.map(vehicle => (
+					<React.Fragment key={vehicle.id}>
+						<div className="col-sm-3">
+							<div className="card" style={{ width: "18rem;" }}>
+								<img
+									className="card-img-top"
+									src="https://via.placeholder.com/150x150"
+									alt="Card image cap"
+								/>
+								<div className="card-body">
+									<h5 className="card-title">{vehicle.name}</h5>
+									<p>{vehicle.model}</p>
+									<p>{vehicle.manufacturer}</p>
+									<a href="#" className="btn btn-primary">
+										Go somewhere
+									</a>
+								</div>
+							</div>
+						</div>
+					</React.Fragment>
+				))}
+			</div>
+		</React.Fragment>
 	);
 };
 export default CardVehicles;
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -8,42 +8,36 @@ const getState = ({ getStore, getActions, setStore }) => {
 		actions: {
 			// Use getActions to call a function within a fuction
 			loadPeople() {
-				const currentStore = getStore();
 				const url = "https://swapi.dev/api/people/";
 				fetch(url)
 					.then(res => res.json())
 					.then(result => {
 						console.log("hola people", result);
 						setStore({
-							...currentStore,
 							people: result.results
 						});
 					})
 					.catch(e => console.error(e));
 			},
 			loadPlanets() {
-				const currentStore = getStore();
 				const url = "https://swapi.dev/api/planets/";
 				fetch(url)
 					.then(res => res.json())
 					.then(result => {
 						console.log("hola planets", result);
 						setStore({
-							...currentStore,
 							planets: result.results
 						});
 					})
 					.catch(e => console.error(e));
 			},
 			loadVehicles() {
-				const currentStore = getStore();
 				const url = "https://swapi.dev/api/vehicles/";
 				fetch(url)
 					.then(res => res.json())
 					.then(result => {
 						console.log("hola vehicles", result);
 						setStore({
-							...currentStore,
 							vehicles: result.results
 						});
 					})
