0c78b09c70d2e3a2d0da0374d4b363c882520e79
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-06-09 13:23:59 +0000

    Se añade la vista SingleCard para people

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -21,11 +21,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 						let itemList = [];
 						if (name in currentStore) {
 							itemList = currentStore[name];
-							console.log(itemList);
+							//console.log(itemList);
 						}
 						for (let item of result.results) {
 							itemList.push(item);
-							console.log(item);
+							//console.log(item);
 						}
 						setStore({
 							[name]: itemList,
@@ -34,6 +34,32 @@ const getState = ({ getStore, getActions, setStore }) => {
 					})
 					.catch(e => console.error(e));
 			},
+			singleCard: (name, id) => {
+				let url = "https://swapi.dev/api/" + name + "/" + id + "/";
+				const currentStore = getStore();
+				fetch(url)
+					.then(res => res.json())
+					.then(result => {
+						let itemSingle = [];
+						if (name in currentStore) {
+							itemSingle = currentStore[name];
+							console.log(itemSingle);
+						}
+						for (let single of result.results) {
+							itemSingle.push(single);
+							console.log(single);
+						}
+						setStore({
+							[name]: itemSingle
+						});
+					})
+					.catch(e => console.error(e));
+			},
+			loadSingleCard: () => {
+				const actions = getActions();
+				actions.singleCard("people", "1");
+			},
+
 			loadPeople: () => {
 				const actions = getActions();
 				actions.getData("people");
@@ -52,10 +78,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 				if (store.favorites.includes(name)) {
 					alert("Item already exist in your list");
 				} else {
-					console.log(`Hello, you are adding ${name}`);
+					//console.log(`Hello, you are adding ${name}`);
 					setStore({ favorites: [...store.favorites, name] });
 				}
-				console.log(store.favorites);
+				//console.log(store.favorites);
 			},
 			deleteFavourite: i => {
 				const store = getStore();
--- src/js/views/SingleCard.js
+++ src/js/views/SingleCard.js
@@ -2,38 +2,60 @@ import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link, useRouteMatch, useParams, useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
-
 export const SingleCard = () => {
 	const { store, actions } = useContext(Context);
 	const match = useRouteMatch();
 	const history = useHistory();
 	const [editName, setName] = useState("");
-
-	useEffect(() => {
-		console.log("running useEffect");
-		for (let contact of store.name) {
-			console.log("checking > ", contact);
-			if (contact.name == match.params.contactId) {
-				console.log("found it!");
-				setName(contact.name);
+	const [height, setHeight] = useState("");
+	const [hairColor, setHairColor] = useState("");
+	//const [editHeight, setHeight] = useState("");
+	useEffect(
+		() => {
+			console.log("running useEffect");
+			console.log(`THIS IS STORE PEOPLE: ${store.people} and ${store.people.length}`);
+			for (let contact of store.people) {
+				console.log("checking > ", contact.name);
+				if (contact.name == match.params.contactId) {
+					console.log("found it!");
+					setName(contact.name);
+					setHeight(contact.height);
+					setHairColor(contact.hair_color);
+					//setHeight(contact.height);
+				}
 			}
-		}
-	}, []);
-
+		},
+		[store]
+	);
 	return (
-		<div className="card" style="width: 18rem;">
-			<img className="card-img-top" src="..." alt="Card image cap" />
-			<div className="card-body">
-				<h5 className="card-title">{editName}</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make up the bulk of the cards content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Go somewhere
-				</a>
+		<div className="row justify-content-md-center">
+			<div className="card-deck">
+				<div className="col">
+					<div className="card bg-light" style={{ width: "18rem" }}>
+						<img className="card-img-top" src="https://via.placeholder.com/150x150" alt="Card image cap" />
+						<div className="card-body">
+							<h5 className="card-title">
+								Name:
+								{editName}
+							</h5>
+							<p>
+								Height:
+								{height}
+							</p>
+							<p>
+								Hair Color:
+								{hairColor}
+							</p>
+							<Link to="/">
+								<button href="#" className="btn btn-primary">
+									Go home
+								</button>
+							</Link>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
 };
-
 SingleCard.propTypes = {};
