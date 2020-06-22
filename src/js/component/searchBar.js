05d34177dd5470b974a37d699eff69cc43569251
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-06-22 20:09:32 +0000

    Se modifica el estilo del SearchBar

--- src/js/component/searchBar.js
+++ src/js/component/searchBar.js
@@ -4,11 +4,27 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const searchBar = () => {
-    const { store, actions } = useContext(Context);
-    const [card, setCard] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [search, setSearch] = useState("");
-    const [filteredCard, setFilteredCard] = useState([]);
+	const { store, actions } = useContext(Context);
+	const [card, setCard] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [search, setSearch] = useState("");
+	const [filteredCard, setFilteredCard] = useState([]);
+	return (
+		<div className="container">
+			<div className="row justify-content-center mt-4 mb-3">
+				<div className="col-5">
+					<div className="input-group">
+						<input type="text" className="form-control" placeholder="Search here..." />
+						<div className="input-group-append">
+							<button className="btn btn-secondary" type="button">
+								<i className="fa fa-search" />
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-}
-
+export default searchBar;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,6 +4,7 @@ import "../../styles/home.scss";
 import CardPeople from "../component/CardPeople";
 import CardVehicles from "../component/CardVehicles";
 import CardPlanets from "../component/CardPlanets";
+import SearchBar from "../component/searchBar";
 
 export const Home = () => (
 	<div className="text-center mt-5">
@@ -13,6 +14,7 @@ export const Home = () => (
 				src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4d2f3d93-e8b3-4e6d-bc04-80d1b99db218/d97g7bt-8fabadb3-b940-48b7-a612-00c4bec89ddd.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNGQyZjNkOTMtZThiMy00ZTZkLWJjMDQtODBkMWI5OWRiMjE4XC9kOTdnN2J0LThmYWJhZGIzLWI5NDAtNDhiNy1hNjEyLTAwYzRiZWM4OWRkZC5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.Zz2vrPSjOIomx3zIY7QfQdTYUuFmfpfmyBVZjsskVFI"
 			/>
 		</div>
+		<SearchBar />
 		<CardPeople />
 		<CardVehicles />
 		<CardPlanets />
